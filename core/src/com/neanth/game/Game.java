ad55a97290698d24862aea426a3a3d7762177353
neant <neanth2014@gmail.com>
2021-03-20 12:05:33 +0300

    Загрузка персонажа из sprite sheet, получение polyline для механизма столкновений

--- core/src/com/neanth/game/Game.java
+++ core/src/com/neanth/game/Game.java
@@ -3,35 +3,41 @@ package com.neanth.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.TextureMapObject;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class Game extends ApplicationAdapter implements InputProcessor {
+public class Game extends ApplicationAdapter {
 	OrthographicCamera camera;
 	TiledMap tiledMap;
 	OrthogonalTiledMapRenderer tiledMapRenderer;
 	Player player;
+	Texture spritesheet;
 	
 	@Override
 	public void create () {
-		Gdx.input.setInputProcessor(this);
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.update();
+		spritesheet = new Texture("spritesheet.png");
 		tiledMap = new TmxMapLoader().load("level1.tmx");
 		MapLayer gameobjects = tiledMap.getLayers().get("gameobjects");
 		for (MapObject object : gameobjects.getObjects()) {
 			switch(object.getName()) {
 				case "Player":
-					player = new Player((TextureMapObject) object);
+					player = new Player((RectangleMapObject) object, spritesheet);
+					break;
+				case "Polyline":
+					PolylineMapObject polyline = (PolylineMapObject) object;
+
 					break;
 				default:
 					break;
@@ -43,6 +49,15 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 
 	@Override
 	public void render () {
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			player.setX(player.getX() - 16 * 3);
+		} else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+			player.setX(player.getX() + 16 * 3);
+		} else if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+			player.setY(player.getY() + 16 * 3);
+		} else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+			player.setY(player.getY() - 16 * 3);
+		}
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		tiledMapRenderer.render();
@@ -56,58 +71,5 @@ public class Game extends ApplicationAdapter implements InputProcessor {
 	public void dispose () {
 		tiledMap.dispose();
 		tiledMapRenderer.dispose();
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		if (keycode == Input.Keys.A) {
-			player.setX(player.getX() - 16 * 3);
-			return true;
-		} else if (keycode == Input.Keys.D) {
-			player.setX(player.getX() + 16 * 3);
-			return true;
-		} else if (keycode == Input.Keys.W) {
-			player.setY(player.getY() + 16 * 3);
-			return true;
-		} else if (keycode == Input.Keys.S) {
-			player.setY(player.getY() - 16 * 3);
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(float amountX, float amountY) {
-		return false;
 	}
 }
--- core/src/com/neanth/game/Player.java
+++ core/src/com/neanth/game/Player.java
@@ -1,13 +1,18 @@
 package com.neanth.game;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.maps.objects.TextureMapObject;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 
 public class Player extends Sprite {
-    public Player(TextureMapObject object) {
-        super(object.getTextureRegion());
-        setSize(16*3, 16*3);
-        setX(object.getX() * 3);
-        setY(object.getY() * 3);
+    public Player(RectangleMapObject object, Texture spritesheet) {
+        int width = (int) object.getRectangle().width;
+        int height = (int) object.getRectangle().height;
+        TextureRegion tr = new TextureRegion(spritesheet, 96, 176, width, height);
+        setRegion(tr);
+        setSize(width * 3, height * 3);
+        setX(object.getRectangle().x * 3);
+        setY(object.getRectangle().y * 3);
     }
 }
